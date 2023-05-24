a19a0db08bafdce1b54845aabe7722853b5410e2
Isaacoun100 <Isaacoun100@gmail.com>
2023-05-24 01:46:20 -0600

    Temporary file to test the text fill in

---
+++ src/app/setValues.ts
@@ -0,0 +1,54 @@
+export const nutri = {
+    nutri_code: "ABC123",
+    id: "1234567890",
+    name: "John",
+    lastname_1: "Doe",
+    lastname_2: "Smith",
+    address: "123 Main Street",
+    photo: "https://picsum.photos/500",
+    email: "john.doe@example.com",
+    birth_date: "1990-01-01",
+    billing_type: "credit",
+    role_name: "user"
+};
+
+export const patients = [
+    {
+        id: "1",
+        name: "John",
+        lastname_1: "Doe",
+        lastname_2: "Smith"
+    },
+    {
+        id: "2",
+        name: "Jane",
+        lastname_1: "Smith",
+        lastname_2: "Johnson"
+    },
+    {
+        id: "3",
+        name: "Michael",
+        lastname_1: "Brown",
+        lastname_2: "Davis"
+    },
+    {
+        id: "4",
+        name: "Sarah",
+        lastname_1: "Johnson",
+        lastname_2: "Wilson"
+    },
+    {
+        id: "5",
+        name: "David",
+        lastname_1: "Lee",
+        lastname_2: "Anderson"
+    }
+]
+
+export const patientDaily = {
+    breakfast: "Oatmeal with mixed berries and a side of Greek yogurt",
+    snack_morning: "A handful of almonds and an apple",
+    lunch: "Grilled chicken breast with quinoa and roasted vegetables",
+    snack_afternoon: "Carrot sticks with hummus",
+    dinner: "Salmon with steamed broccoli and brown rice"
+}
