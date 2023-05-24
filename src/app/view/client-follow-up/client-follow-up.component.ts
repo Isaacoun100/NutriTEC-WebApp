a895d2e399c309cc860ec76f2f099cd3518e15f0
Isaacoun100 <Isaacoun100@gmail.com>
2023-05-24 01:47:20 -0600

    Client Follow Up view added

--- src/app/view/client-follow-up/client-follow-up.component.ts
+++ src/app/view/client-follow-up/client-follow-up.component.ts
@@ -1,4 +1,7 @@
+import { patients } from 'src/app/setValues';
+import { patientDaily } from './../../setValues';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-client-follow-up',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-follow-up.component.scss']
 })
 export class ClientFollowUpComponent {
+
+  patientDaily = patientDaily;
+  patients = patients;
+
+  /**
+   * @description This is the form we use to store the data the user inputs.
+   * @version 1.0
+   */
+  clientForm = new FormGroup({
+    id: new FormControl('', Validators.required)
+  });
+
+  /**
+   * @description This is the form we use to store the data the user inputs.
+   * @version 1.0
+   */
+  commentForm = new FormGroup({
+    comment : new FormControl('', Validators.required)
+  });
+
+  /**
+   * @description This method is used to loads the consumption of set client
+   * @param form 
+   */
+  loadClientInfo(form: any) {
+    console.log(form);
+  }
+
+  /**
+   * @description This method is used to loads the consumption of set client
+   * @param form 
+   */
+  commentClient(form: any) {
+    console.log(form);
+  }
 
 }
