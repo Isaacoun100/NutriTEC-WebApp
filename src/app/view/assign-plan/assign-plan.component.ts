41c08a61ffb7315d0b3a46b000796c61cc92e974
Isaacoun100 <Isaacoun100@gmail.com>
2023-05-25 02:03:52 -0600

    Assign plan view designed
    The assign plan view is now complete

--- src/app/view/assign-plan/assign-plan.component.ts
+++ src/app/view/assign-plan/assign-plan.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { patients, plans } from 'src/app/setValues';
 
 @Component({
   selector: 'app-assign-plan',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./assign-plan.component.scss']
 })
 export class AssignPlanComponent {
+
+  patients = patients;
+  plans = plans;
+
+  assignForm = new FormGroup({
+    patient : new FormControl('', Validators.required),
+    plan : new FormControl('', Validators.required),
+    startDate : new FormControl('', Validators.required),
+    endDate : new FormControl('', Validators.required)
+  });
+
+  assignPlan(form : any) {
+    console.log(form);
+  }
 
 }
