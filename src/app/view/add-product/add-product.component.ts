66342be62c70d8cebb2d42f722eaf676dedbce54
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-02 20:52:33 -0600

    Add Product view completed

--- src/app/view/add-product/add-product.component.ts
+++ src/app/view/add-product/add-product.component.ts
@@ -1,3 +1,5 @@
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { vitamins } from './../../setValues';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,55 @@ import { Component } from '@angular/core';
 })
 export class AddProductComponent {
 
+  vitamins = vitamins;
+  parentSelector: boolean = false;
+
+  productForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    size: new FormControl(0, Validators.required),
+    calories: new FormControl(0, Validators.required),
+    fat: new FormControl(0, Validators.required),
+    sodium: new FormControl(0, Validators.required),
+    carbs: new FormControl(0, Validators.required),
+    protein: new FormControl(0, Validators.required),
+    calcium: new FormControl(0, Validators.required),
+    iron: new FormControl(0, Validators.required),
+    vitamins : new FormArray([
+      new FormControl('', Validators.required)
+    ])
+  });
+
+  addProduct (form : any) {
+
+    this.productForm.controls['vitamins'].clear();
+
+    console.log(this.productForm.controls['vitamins']);
+    
+    for (let index = 0; index < vitamins.length; index++){
+      
+      if (vitamins[index].select == true){
+        this.productForm.controls['vitamins'].push( new FormControl(vitamins[index].vitamin, Validators.required) );
+      }
+
+    }
+
+    console.log(this.productForm.getRawValue());
+
+  }
+
+  onChangeFood($event: any) {
+    const vitamin = $event.target.value;
+    const isChecked = $event.target.checked;
+
+    this.vitamins = this.vitamins.map((d) => {
+      if (d.vitamin == vitamin) {
+        d.select = isChecked;
+        this.parentSelector = false;
+        return d;
+      }
+      return d;
+    });
+    console.log(this.vitamins);
+  }
+    
 }
