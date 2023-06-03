bb46af3c3b37b8fe616033113e10824febe1f260
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-02 20:52:11 -0600

    Add Measurement view complete

--- src/app/view/add-measurement/add-measurement.component.ts
+++ src/app/view/add-measurement/add-measurement.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-measurement',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-measurement.component.scss']
 })
 export class AddMeasurementComponent {
+
+  measurementForm = new FormGroup({
+    weight : new FormControl(0, Validators.required),
+    waist : new FormControl(0, Validators.required),
+    neck : new FormControl(0, Validators.required),
+    hip : new FormControl(0, Validators.required),
+    muscle_percentage : new FormControl(0, Validators.required),
+    client_id : new FormControl('', Validators.required),
+    date : new FormControl('', Validators.required),
+    fat_percentage : new FormControl(0, Validators.required)
+  });
+
+  addMeasurement(form : any) {
+    console.log(form);
+  }
 
 }
