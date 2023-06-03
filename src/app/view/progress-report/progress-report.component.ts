08c5433614642c36e0e5eaed0de863dbd502b222
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-02 20:56:15 -0600

    Progress Report View added

--- src/app/view/progress-report/progress-report.component.ts
+++ src/app/view/progress-report/progress-report.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { clientReports } from 'src/app/setValues';
 
 @Component({
   selector: 'app-progress-report',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./progress-report.component.scss']
 })
 export class ProgressReportComponent {
+
+  clientReports = clientReports;
+
+  progressForm = new FormGroup({
+    client_id : new FormControl('', Validators.required),
+    start_date : new FormControl('', Validators.required),
+    end_date : new FormControl('', Validators.required)
+  });
+
+  getProgress(form : any) {
+    console.log(form);
+  }
 
 }
