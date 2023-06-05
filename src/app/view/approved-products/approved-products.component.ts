f16e3b254184723ddf4c861c08b359e1f0cc8b20
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-05 17:11:06 -0600

    Approved products API connection added

--- src/app/view/approved-products/approved-products.component.ts
+++ src/app/view/approved-products/approved-products.component.ts
@@ -1,23 +1,53 @@
-import { Component } from '@angular/core';
-import { unnaprovedProducts } from 'src/app/setValues';
+import { Component, OnInit } from '@angular/core';
+import { ApproveProductsService } from 'src/app/controler/Admin/approveProducts/approve-products.service';
+import { ProductI, UnnaprovedProductsI } from 'src/app/model/Admin/approve-product';
 
 @Component({
   selector: 'app-approved-products',
   templateUrl: './approved-products.component.html',
   styleUrls: ['./approved-products.component.scss']
 })
-export class ApprovedProductsComponent {
+export class ApprovedProductsComponent implements OnInit {
 
   currentProduct = 0;
-  
-  unnaprovedProducts = unnaprovedProducts
+
+  unnaprovedProducts : UnnaprovedProductsI[] = [];
+
+  constructor( private api: ApproveProductsService ) {}
 
   deleteProduct(){
+
     console.log( "Deleting "+ this.unnaprovedProducts[this.currentProduct].name + "..." )
+    
+    const product = { 
+      product_name: this.unnaprovedProducts[this.currentProduct].name
+    }
+
+    console.log(product);
+
+    this.api.deleteProduct(product).subscribe((data) => {
+      console.log(data);
+      alert('Product deleted successfully');
+    });
+
+    this.updateProducts();
+    
   }
 
   approveProduct(){
     console.log( "Approving "+ this.unnaprovedProducts[this.currentProduct].name + "..." );
+
+    const product = { 
+      product_name: this.unnaprovedProducts[this.currentProduct].name
+    }
+
+    this.api.approveProduct(product).subscribe((data) => {
+      console.log(data);
+      alert('Product approved successfully');
+    });
+
+    this.updateProducts();
+
   }
 
   setCurrentProduct(index : number){
@@ -25,4 +55,15 @@ export class ApprovedProductsComponent {
     console.log(this.currentProduct);
   }
 
+  ngOnInit(): void {
+    this.updateProducts();
   }
+
+  updateProducts(){
+    this.api.getUnnaprovedProducts().subscribe((data) => {
+      console.log(data);
+      this.unnaprovedProducts = data.result;
+    });
+  }
+
+}
