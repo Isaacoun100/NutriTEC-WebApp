612c5eb41df0d9c29dd3f9f0f9e0e4641497b1c7
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-05 16:35:50 -0600

    Assign Plan API connection added

--- src/app/view/assign-plan/assign-plan.component.ts
+++ src/app/view/assign-plan/assign-plan.component.ts
@@ -1,26 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { patients, plans } from 'src/app/setValues';
+import { AssignPlanService } from 'src/app/controler/Nutritionist/assignPlan/assign-plan.service';
+import { NutriClientsI, PlansI, GetNutriClientsI } from 'src/app/model/Nutritionist/get-nutri';
 
 @Component({
   selector: 'app-assign-plan',
   templateUrl: './assign-plan.component.html',
   styleUrls: ['./assign-plan.component.scss']
 })
-export class AssignPlanComponent {
+export class AssignPlanComponent implements OnInit{
 
-  patients = patients;
-  plans = plans;
+  patients : NutriClientsI[] = [];
+  plans : PlansI[] = [];
 
   assignForm = new FormGroup({
-    patient : new FormControl('', Validators.required),
-    plan : new FormControl('', Validators.required),
-    startDate : new FormControl('', Validators.required),
-    endDate : new FormControl('', Validators.required)
+    email_client : new FormControl('', Validators.required),
+    plan_name : new FormControl('', Validators.required),
+    start_date : new FormControl('', Validators.required),
+    end_date : new FormControl('', Validators.required)
   });
 
+  constructor( private api : AssignPlanService ){}
+
   assignPlan(form : any) {
+    
     console.log(form);
+
+    this.api.assignPlan(form).subscribe((data) => {
+      alert('Plan assigned successfully');
+    });
+    
+  }
+
+  ngOnInit(): void {
+
+    const nutritionist : GetNutriClientsI = {'nutritionist_id': JSON.parse(String(sessionStorage.getItem('nutri')))['nutri_code']};
+
+    this.api.getNutriClients(nutritionist).subscribe((data) => {
+      this.patients = data.result;
+    });
+
+    this.api.getNutriPlans(nutritionist).subscribe((data) => {
+      this.plans = data.result;
+    });
+
   }
 
 }
