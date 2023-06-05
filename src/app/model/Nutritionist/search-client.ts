da0056154b925491201f1c7a7510d6983f00a3db
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-05 17:13:03 -0600

    Seach Clients models updated

--- src/app/model/Nutritionist/search-client.ts
+++ src/app/model/Nutritionist/search-client.ts
@@ -1,7 +1,25 @@
 // {
 //   "client_name":"string"
 // }
+// {
+//     "client_email":"string",
+//     "nutritionist_id":"string"
+// }
+// {
+//     "client_name":"string",
+//     "email":"string"
+// }
 
 export interface SearchClientI {
-    client_name: string;
+    client_name: string | null;
+}
+
+export interface AssociateClientI {
+    client_email: string | null;
+    nutritionist_id: string | null;
+}
+
+export interface newClientI {
+    client_name: string | null;
+    email: string | null;
 }
