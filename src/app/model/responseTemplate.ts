1deba848f9c6f78c6199f2d83f0881ad4f41983b
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-05 17:12:13 -0600

    Response template models updated

--- src/app/model/responseTemplate.ts
+++ src/app/model/responseTemplate.ts
@@ -1,4 +1,41 @@
+import { DishItemI, PaymentI, UnnaprovedProductsI } from "./Admin/approve-product";
+import { NutriClientsI, PlansI } from "./Nutritionist/get-nutri";
+import { newClientI } from "./Nutritionist/search-client";
+
 export interface ResponseTemplateI {
     status: string;
     result: JSON;
   }
+
+// Response List
+
+export interface ResponseTemplateListClientI {
+    status: string;
+    result: newClientI[];
+}
+
+export interface ResponseTemplateListPatientI {
+    status: string;
+    result: NutriClientsI[];
+}
+
+export interface ResponseTemplateListPlanI {
+    status: string;
+    result: PlansI[];
+}
+
+export interface ResponseTemplateListReportI {
+    status: string;
+    result: PaymentI[];
+}
+
+export interface ResponseTemplateListUnnaprovedI {
+    status: string;
+    result: UnnaprovedProductsI[];
+}
+
+export interface ResponseTemplateListProductI {
+    status: string;
+    result: DishItemI[];
+}
+
