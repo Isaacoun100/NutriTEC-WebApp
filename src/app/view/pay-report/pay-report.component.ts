bf1e4d83854ba94f4a1fb3f08a4680298e8c4fd9
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-05 16:34:08 -0600

    Pay Report API connection added

--- src/app/view/pay-report/pay-report.component.ts
+++ src/app/view/pay-report/pay-report.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PayReportService } from 'src/app/controler/Admin/payReport/pay-report.service';
+import { PaymentI } from 'src/app/model/Admin/approve-product';
 import { payReports } from 'src/app/setValues';
 
 @Component({
@@ -7,14 +9,21 @@ import { payReports } from 'src/app/setValues';
   templateUrl: './pay-report.component.html',
   styleUrls: ['./pay-report.component.scss']
 })
-export class PayReportComponent {
+export class PayReportComponent implements OnInit{
 
-  payReports = payReports;
+  payReports : PaymentI[] = [];
 
   exportPDF(form : any) {
     console.log(form);
   }
 
+  constructor( private api : PayReportService ) { }
 
+  ngOnInit(): void {
+    this.api.generateAdminReport().subscribe((data) => {
+      console.log(data);
+      this.payReports = data.result;
+    });
+  }
 
 }
