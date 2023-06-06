7fe25a308b2e4e9420774535e5866ff28eeafa37
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-06 02:46:53 -0600

    Client hub completed

--- src/app/view/client-hub/client-hub.component.ts
+++ src/app/view/client-hub/client-hub.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { client, comments, patientDaily } from 'src/app/setValues';
+import { ClientHubService } from 'src/app/controler/Client/clientHub/client-hub.service';
+import { CommentI } from 'src/app/model/Client/load-comments';
+import { DailyFoodI } from 'src/app/model/Nutritionist/follow-up';
 
 @Component({
   selector: 'app-client-hub',
@@ -8,8 +10,38 @@ import { client, comments, patientDaily } from 'src/app/setValues';
 })
 export class ClientHubComponent {
 
-  client = client;
-  patientDaily = patientDaily;
-  comments = comments;
+  patientDaily : DailyFoodI[] = [];
+  comments : CommentI[] = [];
+
+  constructor( private api : ClientHubService ) {
+      
+    this.api.loadComments({client : this.getSessionStorage('email') }).subscribe( data => {
+      console.log(data);
+      this.comments = data;
+    });
+
+    this.api.loadPlan({client_id : this.getSessionStorage('email') }).subscribe( data => {
+      console.log(data.result);
+      this.patientDaily = data.result;
+    });
+
+  }
+
+  getDailyFood(time : string) {
+
+    const food : DailyFoodI[] = [];
+
+    for (let index = 0; index < this.patientDaily.length; index++) {
+      if(this.patientDaily[index].food_time == time){
+        food.push(this.patientDaily[index]);
+      }
+    }
+
+    return food;
+  }
+
+  getSessionStorage( key : string ) {
+    return JSON.parse( sessionStorage.getItem('client') || '{}')[key];
+  }
 
 }
