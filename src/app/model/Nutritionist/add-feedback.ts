5b5d4f285574e80f0a89cdebfcca746bd35e121f
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-06 02:47:55 -0600

    Feedback field fix

--- src/app/model/Nutritionist/add-feedback.ts
+++ src/app/model/Nutritionist/add-feedback.ts
@@ -7,9 +7,9 @@
 // }
 
 export interface FeedbackI {
-    id: string;
-    client: string;
-    nutritionist: string;
-    message: string;
-    date: string;
+    id: string | null;
+    client: string | null;
+    nutritionist: string | null;
+    message: string | null;
+    date: string | null;
 }
