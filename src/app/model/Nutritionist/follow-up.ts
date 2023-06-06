0aef62c589ca0e869a4cc9ca7ac2cb07831b1666
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-06 02:47:24 -0600

    Model fix

--- src/app/model/Nutritionist/follow-up.ts
+++ src/app/model/Nutritionist/follow-up.ts
@@ -6,12 +6,23 @@
 //     "serving":"string",
 //     "dish_name":"string"
 // }
+// {
+//     "serving":"string",
+//     "dish_name":"string",
+//     "food_time":"breakfast"
+// }
 export interface GetDailyI {
-    client_email: string;
-    date: string;
+    client_email: string | null;
+    date: string | null;
 }
 
 export interface AddDailyI {
     serving: string;
     dish_name: string;
+}
+
+export interface DailyFoodI {
+    serving: string | null;
+    dish_name: string | null;
+    food_time: string | null;
 }
