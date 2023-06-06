1656f35754c42bcc62ef86ffa4101f84e57c521e
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-06 02:48:10 -0600

    Add measurements done

--- src/app/view/add-measurement/add-measurement.component.ts
+++ src/app/view/add-measurement/add-measurement.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AddMeasurementService } from 'src/app/controler/Client/addMeasurement/add-measurement.service';
+import { AddMeasurementsI } from 'src/app/model/Client/add-measurements';
 
 @Component({
   selector: 'app-add-measurement',
@@ -8,19 +10,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AddMeasurementComponent {
 
+  constructor( private api : AddMeasurementService) {
+    this.measurementForm.controls['client_id'].setValue(
+      JSON.parse(String(sessionStorage.getItem('client')))['email']
+    );
+   }
+
   measurementForm = new FormGroup({
+    client_id : new FormControl('', Validators.required),
+    date : new FormControl('', Validators.required),
     weight : new FormControl(0, Validators.required),
     waist : new FormControl(0, Validators.required),
     neck : new FormControl(0, Validators.required),
     hip : new FormControl(0, Validators.required),
-    muscle_percentage : new FormControl(0, Validators.required),
-    client_id : new FormControl('', Validators.required),
-    date : new FormControl('', Validators.required),
-    fat_percentage : new FormControl(0, Validators.required)
+    muscle_percentage : new FormControl('0', Validators.required),
+    fat_percentage : new FormControl('0', Validators.required)
   });
 
-  addMeasurement(form : any) {
-    console.log(form);
+  addMeasurement(form : AddMeasurementsI) {
+
+    this.api.addMeasuments(form).subscribe(data => {
+      alert('Measurement added successfully');
+    });
   }
 
 }
