8e1dba4a3e0f593431d3841dba2575bccd2bb2f0
Isaacoun100 <Isaacoun100@gmail.com>
2023-06-06 02:47:11 -0600

    Client Follow Up completed

--- src/app/view/client-follow-up/client-follow-up.component.ts
+++ src/app/view/client-follow-up/client-follow-up.component.ts
@@ -1,25 +1,46 @@
-import { patients } from 'src/app/setValues';
-import { patientDaily } from './../../setValues';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { GetNutriClientsI, NutriClientsI } from 'src/app/model/Nutritionist/get-nutri';
+import { AssignPlanService } from 'src/app/controler/Nutritionist/assignPlan/assign-plan.service';
+import { FollowUpService } from 'src/app/controler/Nutritionist/followUp/follow-up.service';
+import { DailyFoodI, GetDailyI } from 'src/app/model/Nutritionist/follow-up';
+import { FeedbackI } from 'src/app/model/Nutritionist/add-feedback';
 
 @Component({
   selector: 'app-client-follow-up',
   templateUrl: './client-follow-up.component.html',
   styleUrls: ['./client-follow-up.component.scss']
 })
-export class ClientFollowUpComponent {
+export class ClientFollowUpComponent implements OnInit  {
 
-  patientDaily = patientDaily;
-  patients = patients;
+  patientDaily : DailyFoodI[] = [];
+  patients : NutriClientsI[] = [];
+  myDate : string | null = '';
+
+  constructor( private apiPatients : AssignPlanService,
+    private apiFollowUp : FollowUpService,
+    private pipe : DatePipe ){
+      this.myDate = this.pipe.transform( new Date(), 'yyyy-MM-dd');
+    }
+
+  ngOnInit(): void {
+    
+    const nutritionist : GetNutriClientsI = {'nutritionist_id': JSON.parse(String(sessionStorage.getItem('nutri')))['nutri_code']};
+
+    this.apiPatients.getNutriClients(nutritionist).subscribe((data) => {
+      this.patients = data.result;
+    });
+
+  }
 
   /**
    * @description This is the form we use to store the data the user inputs.
    * @version 1.0
    */
   clientForm = new FormGroup({
-    id: new FormControl('', Validators.required),
-    date: new FormControl('2000-01-01', Validators.required),
+    client_email: new FormControl('', Validators.required),
+    date: new FormControl('', Validators.required),
   });
 
   /**
@@ -27,23 +48,55 @@ export class ClientFollowUpComponent {
    * @version 1.0
    */
   commentForm = new FormGroup({
-    comment : new FormControl('', Validators.required)
+    id : new FormControl('', Validators.required),  
+    client: new FormControl('', Validators.required),
+    nutritionist: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+    date: new FormControl('', Validators.required)
   });
 
   /**
    * @description This method is used to loads the consumption of set client
    * @param form 
    */
-  loadClientInfo(form: any) {
-    console.log(form);
+  loadClientInfo(form: GetDailyI) {
+
+    this.apiFollowUp.loadPatientInfo(form).subscribe((data) => {
+      this.patientDaily = data.result;
+      console.log(data.result);
+    });
+
+  }
+
+  getDailyFood(time : string) {
+
+    const food : DailyFoodI[] = [];
+
+    for (let index = 0; index < this.patientDaily.length; index++) {
+      if(this.patientDaily[index].food_time == time){
+        food.push(this.patientDaily[index]);
+      }
+    }
+
+    return food;
   }
 
   /**
    * @description This method is used to loads the consumption of set client
    * @param form 
    */
-  commentClient(form: any) {
+  commentClient(form: FeedbackI) {
+
+    form.date = ''+this.myDate;
+    form.nutritionist = ''+JSON.parse(String(sessionStorage.getItem('nutri')))['nutri_code'];
+    form.client = ''+this.clientForm.value.client_email;
+
     console.log(form);
+
+    this.apiFollowUp.leaveComment(form).subscribe((data) => {
+      alert('Comentario agregado con éxito');
+    });
+
   }
 
 }
